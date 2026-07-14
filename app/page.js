80cd8c4b7cc4030e04cc70a01956ebe97d1993f3
iffi47 <iftikharlion958@gmail.com>
2026-07-14 20:03:10 +0500

    add navigation links to Home component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,29 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-    </main>
-  );
+ const pages = [
+  {
+   title: "Meals",
+   route: "/meals",
+  },
+  {
+   title: "Meals Share",
+   route: "/meals/share",
+  },
+  {
+   title: "Community",
+   route: "/community",
+  },
+ ];
+
+ return (
+  <main>
+   <h1 style={{ color: "white", textAlign: "center" }}>Time to get started!</h1>
+   {pages.map((page) => (
+    <p key={page.title}>
+     <Link href={page.route}>{page.title}</Link>
+    </p>
+   ))}
+  </main>
+ );
 }
