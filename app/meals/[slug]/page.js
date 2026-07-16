4bb10db3665cbf58a9a36cc6d58e9d57ed092017
iffi47 <iftikharlion958@gmail.com>
2026-07-16 11:42:38 +0500

    Implement meal page layout and styles; add meal details and instructions rendering

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,36 @@
-export default async function  MealPage({ params }) {
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
 
-  const{ slug} = await params;
-  return(
-    <>
-      <h1>Meal Page</h1>
-      <h2>{slug}</h2>
-    </>
-  )
+export default async function MealPage({ params }) {
+ const { slug } = await params;
+ const meal = getMeal(slug);
+ meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+ return (
+  <>
+   <header className={classes.header}>
+    <div className={classes.image}>
+     <Image
+      fill
+      src={meal.image}
+      alt="meal-image"
+     />
+    </div>
+    <div className={classes.headerText}>
+     <h1>{meal.title}</h1>
+     <p className={classes.creator}>
+      by<a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+     </p>
+     <p className={classes.summary}>{meal.summary}</p>
+    </div>
+   </header>
+   <main>
+    <p
+     className={classes.instructions}
+     dangerouslySetInnerHTML={{
+      __html: meal.instructions,
+     }}></p>
+   </main>
+  </>
+ );
 }
